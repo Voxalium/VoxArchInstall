6cec8987157a1f7641bd2728b0143ed4b12aa4a2
Voxalium <voxalium@gmail.com>
2024-09-20 16:57:13 +0200

    Init REPO

---
+++ archInstall.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash                                                           
+#                                                           +-------------------------------+
+#                                                           |                               |
+#                                                           |         archInstall.sh        |
+#                                                           |         by Voxa. 2024         |
+#                                                           |                               |
+#                                                           +-------------------------------+
+# TODO: Setup clone from github 
+# --- VARIABLES ---
+
+DEVICE=""
+TIMEZONE=""
+PACKAGES=(
+  "base" "base-devel" 
+  "linux" "linux-headers" "linux-firmware" 
+  "mtools" "dosfstools"
+  "os-prober"
+  "amd-ucode"
+  "efibootmgr"
+  "networkmanager"
+)
+
+# --- TOOLS ---
+
+PAUSE(){                                                    #Pause the script
+  read -p "Press a key to continue"
+}
+
+LIST_DISKS(){                                               #List all disks
+  echo -e "\nList of disks :\n"
+  fdisk -l | grep --color=always "/dev"
+  echo ""
+}
+
+# --- PARTITIONNING ---
+
+PARTITIONNING(){
+  echo -e "--- I. PARTITIONNING ---\n"
+  LIST_DISKS
+  read -p "Select a device to work with : " DEVICE
+  parted $DEVICE mklabel gpt                                #New gpt table
+  parted $DEVICE mkpart esp fat32 1 350                     #New esp partition
+  parted $DEVICE set 1 esp on
+  parted $DEVICE mkpart ext4 350 100%                       #New ext4 partition
+  echo -e "\nDisk $DEVICE partition OK\n"
+}
+
+# --- FORMATING ---
+
+FORMATING(){
+  mkfs.fat -F32 "$DEVICE"1                                  #Format esp
+  mkfs.ext4 "$DEVICE"2                                      #Format ext4
+}
+
+# --- MOUNTING ---
+
+MOUNTING(){
+  mount "$DEVICE"2 /mnt                                     #Mount root to /mnt
+  mount --mkdir "$DEVICE"1 /mnt/boot                        #Mount esp  to /mnt/boot  
+}
+
+# --- BASE INSTALL ---
+#
+BASE_INSTALL(){
+  read -p "Set time zone (eg. Europe/Paris) : " TIMEZONE    #Get the time zone
+  timedatectl set-timezone $TIMEZONE                        #Set the time zone
+  reflector > /etc/pacman.d/mirrorlist                      #Update the mirrors and replace /etc/pacman.d/mirrorlist
+  echo "Mirrors updated"
+  pacstrap -K /mnt ${PACKAGES[*]}                           #Install base packages in /mnt
+  genfstab -U /mnt > /mnt/etc/fstab                         #Generate fstab
+  FSTAB_FILE="/mnt/etc/fstab"
+  sed -i 's/fmask=[0-9]\{4\}/fmask=0077/' $FSTAB_FILE
+  sed -i 's/dmask=[0-9]\{4\}/dmask=0077/' $FSTAB_FILE
+  echo "FSTAB MODIFIED"
+}
+
+# --- CHROOT ---
+CHROOT(){
+  cp chroot.sh packages.sh /mnt                             #Copy scripts to run in /mnt
+  arch-chroot /mnt /bin/bash /chroot.sh                            #Chroot
+}
+
+# --- EXECUTION ---
+
+echo "
++---------------------+
+|                     |
+|     ArchInstall     |
+|     by Voxa         |
+|                     |
++---------------------+"
+
+PARTITIONNING
+FORMATING
+MOUNTING
+BASE_INSTALL
+CHROOT
---
+++ chroot.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash                                                           
+#                                                           +-------------------------------+
+#                                                           |                               |
+#                                                           |         chroot.sh             |
+#                                                           |         by Voxa. 2024         |
+#                                                           |                               |
+#                                                           +-------------------------------+
+
+# --- VARIABLES ---
+
+DEVICE=""
+HOSTNAME=""
+KEYMAP=""
+TIMEZONE=""
+USERNAME=""
+
+# --- TOOLS ---
+
+PAUSE(){                                                    #Pause the script
+  read -p "Press a key to continue"
+}
+
+LIST_DISKS(){                                               #List all disks
+  echo -e "\nList of disks :\n"
+  fdisk -l | grep --color=always "/dev"
+  echo ""
+}
+
+# --- TIME ---
+
+GET_INFO(){
+  read -p "Hostname : " HOSTNAME                            #Get Hostname
+  read -p "Username : " USERNAME                            #Get Username
+  echo "DEVICE :"
+  LIST_DISKS                                                #List Disks
+  read -p "Root partition (for boot manager ) : " DEVICE    #Get Root partition
+  read -p "Time zone (eg. Europe/Paris) : " TIMEZONE        #Get Time Zone
+  read -p "Keymap (eg. fr) : " KEYMAP                       #Get Keymap
+}
+
+# --- TIME ---
+
+SET_TIME(){
+  ln -sf /urs/share/zoneinfo/$TIMEZONE /etc/localtime       #Set Timezone
+  hwclock --systohc                                         #Set Clock
+}
+
+# --- LOCALES ---
+
+SET_LOCALES(){
+  echo "en_US.UTF-8 UTF-8" > /etc/locale.gen                #Set Locales.gen to English
+  locale-gen                                                #Local Generation
+  echo "LANG=en_US.UTF-8"  > /etc/locales.conf              #Set Locales to English
+  echo "KEYMAP=$KEYMAP"    > /etc/vconsole.conf           #Set Keymap
+}
+
+# --- NETWORK ---
+
+SET_NETWORK(){
+  echo $HOSTNAME > /etc/hostname                            #Set Hostname
+  echo "                                                    
+  127.0.0.1   localhost
+  ::1         localhost
+  127.0.1.1   $HOSTNAME.localdomain   $HOSTNAME
+  " > /etc/hosts                                            #Config Hosts
+}
+
+# --- BOOT ---
+
+SET_BOOT_MANAGER(){
+  bootctl --path=/boot install                              #Install systemd-boot manager
+  echo "
+  default arch.conf
+  timeout 3  
+  " > /boot/loader/loader.conf                              #Config loader.conf
+  echo "  
+  title Arch Linux
+  linux /vmlinuz-linux
+  initrd /initramfs-linux.img
+  options root=$DEVICE rw
+  " > /boot/loader/entries/arch.conf                        #Config Entries
+
+}
+
+# --- ROOT ---
+
+SET_ROOT_PASSWORD(){                                        #Set Root password
+  echo "Set root password"
+  passwd
+}
+
+# --- USER ---
+
+CREATE_USER(){                                              #Create new user
+  useradd -m -G "wheel" $USERNAME
+  passwd $USERNAME
+  # TODO: SETUP WHEEL in /etc/sudoers 
+}
+
+# --- ADDITIONAL PACKAGES ---
+
+INSTALL_PACKAGES(){                                         #Install additional packages
+  ./packages.sh
+}
+
+# --- SERVICES ---
+
+ENABLE_SERVICES(){
+  systemd enable NetworkManager                             #Enable Network Manager
+
+}
+
+# --- CLEAN ---
+
+CLEAN(){                                                    #Remove scripts
+  rm /chroot.sh /packages.sh
+}
+
+# --- EXECUTION ---
+
+GET_INFO
+SET_TIME
+SET_LOCALES
+SET_NETWORK
+SET_BOOT_MANAGER
+SET_ROOT_PASSWORD
+CREATE_USER
+INSTALL_PACKAGES
+CLEAN
+  
+# TODO: service to enable
+# GDM
